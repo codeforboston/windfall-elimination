8b7d6cb9104b800dd31071ca85fa05046d33c315
Alex Hughes <alxhghs@gmail.com>
2019-03-26 21:17:50 -0400

    remove unused imports

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import Input from "../components/input";
 
