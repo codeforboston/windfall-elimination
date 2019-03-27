3ad631b3854b30186cb6baab7a0f2bdc235c70ba
Alex Hughes <alxhghs@gmail.com>
2019-03-27 10:52:34 -0400

    fix import typo

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-import { breakPoints } from './breakPoints';
+import { breakPoints } from './breakpoints';
 import { colors } from "./colors";
 import { fonts } from './fonts'; 
 import { fontSizes } from './font-sizes';
