939b3e2d3aff0056b98f7f8d2cba9bc482df0ada
LucasLombardo <lucasalombardo@gmail.com>
2019-04-06 15:03:19 -0400

    adjust styling on index page
    Adjust styling on index page of links and change the title
    - Remove height from button-link component
    - Change link-wrapper to work with button-link component as well as
      regular links

--- src/components/button-link.tsx
+++ src/components/button-link.tsx
@@ -5,7 +5,6 @@ import { spacing, colors, fontSizes, radii } from "../constants";
 export const ButtonLink = styled(Link)`
     padding: ${spacing[1]} ${spacing[2]};
     margin: ${spacing[2]} ${spacing[2]};
-    height: 30px;
     background-color: ${colors.blue};
     font-size: ${fontSizes[1]};
     border-radius: ${radii[2]};
--- src/components/link-wrapper.ts
+++ src/components/link-wrapper.ts
@@ -2,9 +2,9 @@ import styled from "@emotion/styled";
 import { spacing } from "../constants";
 
 export const LinkWrapper = styled("div")`
-    display: grid;
-    justify-self: center;
-    justify-content: center;
-    width: 100%;
-    gap: ${spacing[1]};
+    a {
+        display: block;
+        margin: ${spacing[1]} auto;
+        width: fit-content;
+    }
 `;
