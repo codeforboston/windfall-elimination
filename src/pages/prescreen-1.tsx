58329222715764e047de2294e5bd31295ed4ca0c
Michael Caveney <dylanesque27@gmail.com>
2019-04-10 16:31:33 -0400

    Corrected minor grammatical errors in prescreen1 component

--- src/pages/prescreen-1.tsx
+++ src/pages/prescreen-1.tsx
@@ -9,8 +9,8 @@ export default () => (
             The Windfall Elimination Provision is a policy intended to reduce Social Security benefits for people who also have access to a pension or retirement account from work where they did not pay into Social Security.
         </TextBlock>
         <TextBlock>
-            Most jobs that do not pay into social security are jobs with state and local government
-            For six states (including Massachusetts, Kentucky, Ohio, Indiana, Texas, and more), the majority of state and local employees will be affected by WEP; however, for 26 other states, (including XYZ), some but not all retirees will be affected. This means that the majority of affected people are teachers, firefighters, city hall employees, janitors, and other public servants. While the Social Security administration provides some information on how WEP is calculated, this information is often difficult to find and harder to understand.
+            Most jobs that do not pay into social security are jobs with state and local government.
+            For six states (including Massachusetts, Kentucky, Ohio, Indiana, Texas, and more), the majority of state and local employees will be affected by WEP. However, for 26 other states (including XYZ), some but not all retirees will be affected. This means that the majority of affected people are teachers, firefighters, city hall employees, janitors, and other public servants. While the Social Security administration provides some information on how WEP is calculated, this information is often difficult to find and harder to understand.
         </TextBlock>
         <TextBlock>
             On Congressman Moulton’s constituent services team, we believe that government should be easy to use, easy to access, and easy to understand. This is why we teamed up with Code for Boston to help retirees and their families understand how the Windfall Elimination Provision formula works, predict the amount of their retirement benefits, and effectively advocate for themselves with the Social Security Administration.
