c0aa9a24c3edced619f78602b95c453d18089ef5
LucasLombardo <lucasalombardo@gmail.com>
2019-04-11 07:12:17 -0400

    revert link-wrapper changes

--- src/components/link-wrapper.ts
+++ src/components/link-wrapper.ts
@@ -2,9 +2,9 @@ import styled from "@emotion/styled";
 import { spacing } from "../constants";
 
 export const LinkWrapper = styled("div")`
-    a {
-        display: block;
-        margin: ${spacing[1]} auto;
-        width: fit-content;
-    }
+    display: grid;
+    justify-self: center;
+    justify-content: center;
+    width: 100%;
+    gap: ${spacing[1]};
 `;
