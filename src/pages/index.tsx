721d10dc485efe3779dca23dd853cf5212447ad2
Thad Kerosky <contact@thadk.net>
2019-04-26 02:07:09 -0400

    put our observablehq on the front page for now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,40 @@
-import React from "react"
+import React, {Component} from "react";
 import { Link } from "gatsby"
 import { LinkWrapper, SEO, ButtonLink } from "../components";
-
-export default () => (
-    <>
-        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-        <h2>Windfall Elimination Awareness Demo</h2>
-        <p>This is a demo of the Windfall Elimination Project.<br/>Check out our demo or the admin page!</p>
-        <LinkWrapper>
-            <ButtonLink to="/prescreen-1/">Demo</ButtonLink>
-            <Link to="/admin/">Admin</Link>
-            <a href="https://github.com/codeforboston/windfall-elimination">Github Repo</a>
-        </LinkWrapper>
-    </>
-)
+import {Runtime, Inspector} from "@observablehq/notebook-runtime";
+// import notebook from "how-to-embed-a-notebook-in-a-react-app";
+import notebook from "windfall-awareness-notebook-prototype";
+export default class App extends Component {
+    birthdateRef = React.createRef();
+    calculationDisplayRef = React.createRef();
+  
+    componentDidMount() {
+      Runtime.load(notebook, (cell) => {
+        if (cell.name === "birthDatePicked") {
+          return new Inspector(this.birthdateRef.current);
+        }
+        if (cell.name === "calculationDisplay") {
+            return new Inspector(this.calculationDisplayRef.current);
+          }
+       });
+    }
+  
+    render() {
+      return (
+        <>
+             <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+            <h2>Windfall Elimination Awareness Demo</h2>
+            <p>This is a demo of the Windfall Elimination Project.<br/>Check out our demo or the admin page!</p>
+            <LinkWrapper>
+                <ButtonLink to="/prescreen-1/">Demo</ButtonLink>
+                <Link to="/admin/">Admin</Link>
+                <a href="https://github.com/codeforboston/windfall-elimination">Github Repo</a>
+            </LinkWrapper>
+            <p>
+                <div ref={this.birthdateRef}></div>
+                <div ref={this.calculationDisplayRef}></div>
+            </p> 
+        </>
+      );
+    }
+  }
