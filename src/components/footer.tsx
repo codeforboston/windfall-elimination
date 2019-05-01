3f40a7cc19fc14edaed9ceb17cc802ee0dd75293
Prayuth Naduthota <prayuth.naduthota@bullhorn.com>
2019-04-30 20:34:45 -0400

    initial UI edits, including styling updates and adding the about and congrats page

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import styled from "@emotion/styled";
 import { colors } from "../constants";
 
-const Wrapper = styled("header")`
+const Wrapper = styled("footer")`
     background: ${colors.darkGreen};
     display: flex;
     justify-content: center;
