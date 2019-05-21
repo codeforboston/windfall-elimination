d886ab044f033df6baf395170a5492bef4d07b4e
Michael Caveney <dylanesque27@gmail.com>
2019-05-21 14:52:31 -0400

    Added target: __blank to external links, and fixed spacing on select P elements to improve code readability

--- src/pages/prescreen-1c.tsx
+++ src/pages/prescreen-1c.tsx
@@ -6,7 +6,8 @@ export default () => (
         <SEO title="Pre-Screen" keywords={[`gatsby`, `application`, `react`]} />
         <h2>What do I need?</h2>
         <TextBlock>
-            To calculate your WEP-adjusted Benefit amount, we will need your assistance in providing relevant Social Security information.
+            To calculate your WEP-adjusted Benefit amount, we will need your
+            assistance in providing relevant Social Security information.
         </TextBlock>
         <TextBlock>
             Before you begin, please have at your disposal:
@@ -16,7 +17,8 @@ export default () => (
             <li>Earnings Record</li>
             <TextBlock>
                     The above items can be obtained from the Social Security website.
-                    If you are unsure on how to obtain your Earnings Record, instructions can be found <LinkButton to="/prescreen-2/">Here</LinkButton>
+                    If you are unsure on how to obtain your Earnings Record, instructions
+                    can be found <LinkButton to="/prescreen-2/">Here</LinkButton>
             </TextBlock>
             <li>Birthdate</li>
             <li>Retirement Date (year and month)</li>
