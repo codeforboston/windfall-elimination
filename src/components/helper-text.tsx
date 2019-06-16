434a71b7e5dea94531dc7024086a9c5db5e620e5
Michael Caveney <dylanesque27@gmail.com>
2019-06-16 10:34:25 -0400

    edited components for improved html semantics

--- src/components/helper-text.tsx
+++ src/components/helper-text.tsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 import { fontSizes, spacing } from "../constants";
 
-export const HelperText = styled("div")`
+export const HelperText = styled("p")`
   font-size: ${fontSizes[0]};
   max-width: 500px;
   text-align: left;
--- src/components/question-text.tsx
+++ src/components/question-text.tsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 import { fontSizes, spacing } from "../constants";
 
-export const QuestionText = styled("h2")`
+export const QuestionText = styled("h3")`
   font-size: ${fontSizes[1]};
   margin-top: ${spacing[1]};
   width: 75vw;
