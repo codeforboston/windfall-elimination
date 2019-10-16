b609278a4ba850c6bb5ed6b879dd81a879c03b23
Sebastian Upjohn <sebastianupjohn@gmail.com>
2019-10-15 20:42:27 -0400

    Added the getAIMEFromEarningsSimple() function... again

--- src/library/observable-functions.js
+++ src/library/observable-functions.js
@@ -260,6 +260,161 @@ function getRawEarnings(earningsRecord) {
   return rawearnings
 }
 
+
+/* *getAIMEFromEarningsSimple()** Calculates the beneficiary's AIME from their earnings records while estimating future years
+ Must replace getAIMEFromEarnings for the site to be predictive and allow it to be of use to our primary demographic
+ Requires three new parameters: lastRealEarningYear, finalWorkYear, and indexingYearAverage -
+ 	however, we should be able to get this information without changing the interface, as follows:
+ 	* lastRealEarningYear *can* be inferred from looking at the rawEarnings table, though it may be better, moving forward, to ask the user for this
+ 	* We can assume finalWorkYear to be the year before the user retires, which we collect for other functions
+ 	* indexingYearAverage can be also be derived by looking up indexingYear in future entries of avgWageIndexTable
+
+Parameters:
+
+	* rawEarnings, a key-value store for each year as a key, containing earnings as a string.
+	* indexingYear, the second year before the beneficiary's year of attainment of age 62
+	* maxECTable, an array of objects containing a year, the maximum earnings creditable for that year
+	* avgWageIndexTable: the average earnings across all workers in that year
+
+**Returns**: AIME, representing the user's AIME, which is rounded down to an integer
+*/
+
+
+function getAIMEFromEarningsSimple(rawEarnings, indexingYear, maxECTable, avgWageIndexTable, lastRealEarningYear, finalWorkYear, indexingYearAverage) {
+  
+  //Determine # of calculation years
+  let numCalculationYears = 0;
+  if (indexingYear >= 1989) numCalculationYears = 35;
+  else if (indexingYear <= 1956) numCalculationYears = 0;
+  else numCalculationYears = 35 - (1989-indexingYear);
+
+  
+  //convert earnings from key-value's with strings inside, to object format with numbers inside.
+  const earnings = Object.keys(rawEarnings).map(n => ({year: parseInt(n, 10), amount: parseInt(rawEarnings[n], 10)}))
+  
+  let earningsMap = {}; // This map will contain key-value pairs of year-amount earned (for the user)
+  if (Array.isArray(earnings)) {
+    earnings.forEach((earning) => {
+      earningsMap[earning.year] = earning.amount;
+    });
+  } //throw new Error("") - but what would this error be?
+
+  let averageMap = {}; //This map will contain key-value pairs of year-average earnings from table
+  if (Array.isArray(avgWageIndexTable)) {
+    avgWageIndexTable.forEach((earning) => {
+      if (earning.year > 1950 && (earningsMap[earning.year] !== undefined || earning.year >= indexingYear-39 || earning.year < indexingYear+2)) {
+        averageMap[earning.year] = avgWageIndexTable.find(d => d.year === earning.year).averageWageIndex;
+      }
+    });
+  }//throw new Error("") - but what would this error be?
+  
+  //return averageMap
+
+  let maximumMap = {}; //This map will contain key-value pairs of year-maximum from table
+  if (Array.isArray(maxECTable)) {
+    earnings.forEach((earning) => {
+      if (earning.year >= 1950 && earning.year >= indexingYear-39 && (earning.year < indexingYear+2 || earningsMap[earning.year] !== undefined)) {
+        maximumMap[earning.year] = maxECTable.find(d => d.year === earning.year).maximumEarningsCreditable;
+      }
+    });
+  }//throw new Error("") - but what would this error be?
+
+  //Check that averageMap and maximumMap have the same length and set of keys?
+
+
+  //adjust each year's earning to match the indexing year, then add the modified yearly earnings to an array
+  let earningsYears = earnings.map(n => n.year);
+  let validEarnings = []; //array of each year of valid earnings;
+                          //specific year or ordering ceases to be relevant
+  let debugValidEarnings = [];
+
+
+
+  if (Array.isArray(earningsYears)) {
+    earningsYears.forEach((earningsYear) => {
+      if (earningsYear =< lastRealEarningYear) {
+      	return;
+      }
+
+      if (earningsYear !== indexingYear && earningsYear !== NaN && maximumMap[earningsYear] !== undefined && averageMap[earningsYear] !== undefined) {
+        debugValidEarnings.push({
+          'yr': earningsYear,
+          'specialIndexingYear': false,
+          'indexOnYear': indexingYear,
+          'eMap':earningsMap[earningsYear],
+          'mxMap': maximumMap[earningsYear],
+          'min':  Math.min(earningsMap[earningsYear], maximumMap[earningsYear]), //* averageMap[indexingYear],
+          'minSafe':  Math.min(earningsMap[earningsYear], isNaN(maximumMap[earningsYear]) ? 0 : maximumMap[earningsYear]),
+          'avgMapIndex': averageMap,
+          'avgMapYr': averageMap[earningsYear],
+          'validEarningCalc': Math.min(earningsMap[earningsYear], maximumMap[earningsYear]) * indexingYearAverage / averageMap[earningsYear]
+        });
+        // validEarnings.push(Math.min(earningsMap[earningsYear], maximumMap[earningsYear]) * averageMap[indexingYear] / averageMap[earningsYear])
+      }
+      else if (earningsYear === indexingYear && earningsYear !== NaN && maximumMap[earningsYear] !== undefined && averageMap[earningsYear] !== undefined) {
+        //TODO(tdk): what is this if statement for exactly? check with the narrative.
+         debugValidEarnings.push({
+          'yr': earningsYear,
+          'specialIndexingYear': true,
+          'eMap':earningsMap[earningsYear],
+          'mxMap': maximumMap[earningsYear],
+          'min':  Math.min(earningsMap[earningsYear], maximumMap[earningsYear]), //* averageMap[indexingYear],
+          'minSafe':  Math.min(earningsMap[earningsYear], isNaN(maximumMap[earningsYear]) ? 0 : maximumMap[earningsYear]),
+          'avgMapIndex': averageMap[indexingYear],
+          'avgMapYr': averageMap[earningsYear],
+          'validEarningCalc': Math.min(earningsMap[earningsYear], maximumMap[earningsYear])// * averageMap[indexingYear] / averageMap[earningsYear]
+           // Commented out last part - this quantity should always be 1
+        });
+        
+        //validEarnings.push(
+        //  Math.min(earningsMap[earningsYear], maximumMap[earningsYear])
+        //  ); //I'm not sure if this correctly handles situations where a year is missing in earningsMap
+      }
+      else if (earningsYear > indexingYear && earningsYear !== NaN && maximumMap[earningsYear] !== undefined && averageMap[earningsYear] !== undefined) {
+        debugValidEarnings.push({
+          'yr': earningsYear,
+          'specialIndexingYear': false,
+          'eMap':earningsMap[earningsYear],
+          'mxMap': maximumMap [earningsYear],
+          'min': Math.min(earningsMap[earningsYear], maximumMap[earningsYear]),
+          'minSafe': Math.min(earningsMap[earningsYear], isNaN(maximumMap[earningsYear]) ? 0 :
+                          maximumMap[earningsYear]),
+          'avgMapIndex': averageMap[indexingYear],
+          'avgMapYr': averageMap[indexingYear],
+          'validEarningCalc': Math.min(earningsMap[earningsYear], maximumMap[earningsYear]) //Years after indexing year are not indexed; this is favorable to beneficiaries
+        });
+      }
+    });
+    for (int i = lastRealEarningYear+1; i < finalWorkYear+1; i++) {
+      debugValidEarnings.push({
+        'yr': i,
+        'specialIndexingYear': i == indexingYear,
+        'validEarningCalc':Math.min(earningsMap[lastRealEarningYear], maximumMap[lastRealEarningYear]) * 
+        averageMap[lastRealEarningYear] / indexingYearAverage
+      });
+    }
+  }
+
+
+  //Remove members of validEarnings until length matches the number of calculation years
+  const toCut = debugValidEarnings.length-numCalculationYears;
+  if (toCut >= 0) {
+    validEarnings = debugValidEarnings.sort((m, n) => m.validEarningCalc - n.validEarningCalc).slice(toCut).map(n => n.validEarningCalc);
+  } else {
+    validEarnings = debugValidEarnings;
+  }
+  
+  //Sum the earning into AIME
+  let AIME = 0; //Starts at 0, to facilitate later summation
+
+  validEarnings.forEach((earning) => {
+    AIME += earning;
+  });
+  //Divide AIME by number of months ()
+  AIME = AIME / (validEarnings.length * 12);
+  return Math.floor(AIME);
+}
+
 //----------------------------------------------------------------------------------
 
 
