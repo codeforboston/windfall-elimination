cc1ac091a99cb837b420bf483ee1b5886ed4cf7d
Thad Kerosky <contact@thadk.net>
2019-12-25 08:07:27 -0500

    align tests with observable notebook and Official AnyPIA

--- src/test/Observable.test.js
+++ src/test/Observable.test.js
@@ -13,28 +13,47 @@ describe("John Q. Public (Full Retirement)", () => {
 
 	const earnings = fullRetirementValues['osss:OnlineSocialSecurityStatementData']['osss:EarningsRecord']['osss:Earnings'];
 	const userDOB = new Date("1947-10-10");
-	const userDOR = new Date("2014-10-10").toLocaleDateString("en-US");
+	const userDOR = new Date("2013-10-10").toLocaleDateString("en-US"); // 66 is their full retirement age
 	const year62 = "2009";
 	const userYSE = getYearsSE(earnings);
 	const userPension = 0;
 
+
+	it("Correctly tallies years of substantial earnings from a full earnings record.", async () => {
+		expect.assertions(1);
+		
+		expect(userYSE).toBe(30)
+	})
+
 	it("Correctly calculates getAIMEFromEarnings from a full earnings record.", async () => {
 		expect.assertions(1);
 	    
-	  	var AIME = await getAIMEFromEarnings(earnings, 2014)
-
-	    expect(AIME).toBe(3711)
+	  	var AIME = await getAIMEFromEarnings(earnings, year62)
+		//3383 is value for Observable notebook with correct birthdate.
+	    expect(AIME).toBe(3383)
   	})
 
-	it("Correctly calculates Payable Benefit from a full earnings record.", async () => {
+	it("Correctly calculates Payable Benefit from a full earnings record according to observable (good).", async () => {
  		expect.assertions(1);
 
 	  	var userAIME = getAIMEFromEarnings(earnings, year62)
 
 	   	var userCalc = await finalCalculation(userDOB, userDOR, year62, userYSE, userPension, userAIME)
 
-	    expect(Number(userCalc["MPB"])).toBe(1635.21)
+		/* detailed calculator says 1,595.10 while observable says 1514.08, XML said 1811.00 (likely flawed)  */
+	    expect(Number(userCalc["MPB"])).toBe(1514.08)
 	 })
+
+	/* it("Correctly calculates Payable Benefit from a full earnings record (best, XML may be flawed)", async () => {
+		expect.assertions(1);
+
+		 var userAIME = getAIMEFromEarnings(earnings, year62)
+
+		  var userCalc = await finalCalculation(userDOB, userDOR, year62, userYSE, userPension, userAIME)
+
+		// detailed calculator says 1,595.10 while observable says 1514.08, XML said 1811.00 (likely flawed) 
+		expect(Number(userCalc["MPB"])).toBe(1595.10)
+	}) */
 })
 
 describe("Sample 20 AnyPIA (Full Retirement)", () => {
@@ -42,11 +61,16 @@ describe("Sample 20 AnyPIA (Full Retirement)", () => {
 	//Background finance info for Sample20 
 	const earnings = sample20RetirementValues['osss:OnlineSocialSecurityStatementData']['osss:EarningsRecord']['osss:Earnings'];
 	const userDOB = new Date("1952-06-22");
-	const userDOR = new Date("2014-07-22").toLocaleDateString("en-US");
+	const userDOR = new Date("2018-07-22").toLocaleDateString("en-US"); // 66yo is their full retirement age.
 	const year62 = "2014";
 	const userYSE = getYearsSE(earnings);
 	const userPension = 1500;
 
+	it("Correctly tallies years of substantial earnings from a full earnings record.", async () => {
+		expect.assertions(1);
+		
+		expect(userYSE).toBe(22)
+	})
 
 	//Test AIME calculations
 	it("Correctly calculates getAIMEFromEarnings from a full earnings record.", async () => {
@@ -58,14 +82,15 @@ describe("Sample 20 AnyPIA (Full Retirement)", () => {
   	})
 
 	//Test MPB calculations
-	it("Correctly calculates Payable Benefit from a full earnings record.", async () => {
+	it("Correctly calculates Payable Benefit from a full earnings record (best).", async () => {
  		expect.assertions(1);
 
 	  	var userAIME = getAIMEFromEarnings(earnings, year62)
 
 	   	var userCalc = await finalCalculation(userDOB, userDOR, year62, userYSE, userPension, userAIME)
 
-	    expect(Number(userCalc["MPB"])).toBe(1336.47)
+		/* both Detailed Calculator and Observable matched this value for me with the 1500 pension -tk */
+	    expect(Number(userCalc["MPB"])).toBe(1235.84)
 	 })
 })
 
