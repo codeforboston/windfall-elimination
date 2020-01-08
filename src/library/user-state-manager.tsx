5df65ec4740249ee0794a630a2d24c3bcefa0c34
Paavan Bhavsar <paavan@kensho.com>
2020-01-08 17:20:57 -0500

    Fix lint errors and use helper methods to grab start of day using dayjs

--- src/library/user-state-manager.tsx
+++ src/library/user-state-manager.tsx
@@ -1,41 +1,30 @@
 import React from 'react'
 import createPersistedState from 'use-persisted-state';
+import dayjs from 'dayjs'
 
 import {UserStateContextProvider, UserState} from './user-state-context'
 import {UserStateActions, UserStateActionsContextProvider} from './user-state-actions-context'
 
 // Must use sessionStorage (not localStorage) or else it conflicts with other uses of sessionStorage within app
-const useBirthDateState = createPersistedState<Date | null>('BirthDate', global.sessionStorage);
-const useRetireDateState = createPersistedState<Date | null>('RetireDate', global.sessionStorage);
+const useBirthDateState = createPersistedState('BirthDate', window.sessionStorage);
+const useRetireDateState = createPersistedState('RetireDate', window.sessionStorage);
+
+function startOfDay(date: Date): Date {
+  return dayjs(date).startOf('day').toDate()
+}
 
 interface UserStateManagerProps {
   children: React.ReactNode
 }
 
-interface UserStateManagerState {
-  userState: UserState
-}
-
-/**
- * Return a date-setting function which, when given a Date, calls the provided set function with
- * the ISO8601 string representing the start of the day.
- */
-function createDateSetter(setFn: (dateStr: string) => void): ((d: Date) => void) {
-  return (date: Date): void => {
-    const startOfDay = new Date(date)
-    startOfDay.setHours(0, 0, 0, 0)
-    setFn(startOfDay.toISOString())
-  }
-}
-
 export default function UserStateManager(props: UserStateManagerProps): JSX.Element {
   const {children} = props
-  const [birthDate, setBirthDate] = useBirthDateState(null)
-  const [retireDate, setRetireDate] = useRetireDateState(null)
+  const [birthDate, setBirthDate] = useBirthDateState<Date | null>(null)
+  const [retireDate, setRetireDate] = useRetireDateState<Date | null>(null)
 
   const actions: UserStateActions = {
-    setBirthDate: createDateSetter(setBirthDate),
-    setRetireDate: createDateSetter(setRetireDate)
+    setBirthDate: date => setBirthDate(startOfDay(date)),
+    setRetireDate: date => setRetireDate(startOfDay(date)),
   }
 
   const userState: UserState = React.useMemo(() => ({
