8bd54c2f9223a6084ff06199e08fac12fc710553
Thad Kerosky <contact@thadk.net>
2020-01-17 12:05:10 -0500

    improve print page navigation

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -114,8 +114,8 @@ const Layout = ({ children }) => (
             if(location.pathname === "/print/"){
               return (
                 <ButtonContainer>
-                 <ButtonLinkGreen to="/prescreen-1c/">Previous: Employment Status</ButtonLinkGreen>
-                 <ButtonLink to="/">Go Home</ButtonLink>
+                 <ButtonLinkGreen to="/prescreen-1c/">Return to results</ButtonLinkGreen>
+                 <ButtonLink to="/screen-2a/">Continue to Benefit Formula</ButtonLink>
                 </ButtonContainer>
               )
             }
