9dafa2e48b2eb6b7ca0739a718a60ee740321cd0
Thad Kerosky <contact@thadk.net>
2020-02-25 21:40:37 -0500

    Update src/layouts/index.tsx

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import {css} from '@emotion/core'
 import styled from "@emotion/styled";
 import { StaticQuery, graphql } from "gatsby";
 import { Location } from "@reach/router";
