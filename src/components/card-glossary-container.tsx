12d64391ca939109762fd987f074d853c183f5e8
Thad Kerosky <contact@thadk.net>
2020-09-09 04:49:35 -0400

    add forgotten files

---
+++ src/components/card-glossary-container.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+const CardGlossaryContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: auto 0;
+  
+  @media (max-width: 767px){
+    display: block;
+  }
+`;
+
+export default CardGlossaryContainer;
---
+++ src/components/content-container.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+const ContentContainer = styled.div`
+  width: 100%;
+
+`;
+
+  //from screen-2
+  //   @media (min-width: 768px) {
+//     width: 70%;
+//   
+export default ContentContainer;
