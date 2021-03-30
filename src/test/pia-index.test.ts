e94462bcf24655d3400a04d1351c4966a08ae290
Thad Kerosky <contact@thadk.net>
2021-03-30 19:54:51 -0400

    first draft of versioned detailed calculator

--- src/test/pia-index.test.ts
+++ src/test/pia-index.test.ts
@@ -1,46 +1,71 @@
  // TODO ENABLE NODEJS mode for Emscripten Module and uncomment this file  
  // will not work without this. Also need to find way to reference wasm file for tests since /anypiajs.wasm won't workA
 
-// import { finalCalculation } from "../library/pia/index";
-// import { delayedRetirementValues, fullRetirementValues, sample20RetirementValues } from "../library/testFiles";
-// import {
-//   finalCalculation as originalFinalCalculation,
-//   getRawEarnings,
-// } from "../library/observable-functions";
+import { finalCalculation } from "../library/pia/index";
+import { delayedRetirementValues, fullRetirementValues, sample20RetirementValues } from "../library/testFiles";
+import {
+  finalCalculation as originalFinalCalculation,
+  getRawEarnings,
+} from "../library/observable-functions";
+import dayjs from "dayjs";
+
+
 
 describe("Run AnyPIAJS", () => {
-    it("once it is recompiled with NodeJS support", async () => {
+    it("Now it has NodeJS support", async () => {
     expect.assertions(1);
 
     expect(null).toBe(null);
   });
 });
+// TODO:
+// Do an approach like this: https://github.com/emscripten-core/emscripten/issues/8400#issuecomment-498218291
+describe("John Q. Public (Full Retirement)", async () => {
 
-// describe("John Q. Public (Full Retirement)", async () => {
-//   const earnings =
-//     fullRetirementValues["osss:OnlineSocialSecurityStatementData"][
-//       "osss:EarningsRecord"
-//     ]["osss:Earnings"];
-//   const userDOB = new Date("1947-10-10");
-//   const userDOR = new Date("2013-10-10"); // 66 is their full retirement age
-//   const rawEarnings = getRawEarnings(earnings);
-//   const userPension = 0;
+  beforeAll(async () => {
+    var loader = ???();
+    loader.ready = () =>
+        // https://github.com/emscripten-core/emscripten/issues/5820
+        new Promise((resolve, reject) => {
+            delete loader.then;
+            loader.onAbort = reject;
+            loader.addOnPostRun(() => {
+                resolve(loader);
+            });
+        });
+    ??? = await loader.ready();
+});
 
-//   const piaUserCal = await finalCalculation(
-//     userDOB,
-//     userDOR,
-//     userPension,
-//     rawEarnings
-//   );
-//   // var userCalc = await finalCalculation(userDOB, userDOR, year62, userYSE, userPension, userAIME)
+  const earnings =
+    fullRetirementValues["osss:OnlineSocialSecurityStatementData"][
+      "osss:EarningsRecord"
+    ]["osss:Earnings"];
 
-//   const userYSE = Number(piaUserCal["yearsSubstantialEarnings"]);
+    const userDOB = "1947-10-10";
+    const userDOR = dayjs("2013-10-10").toDate(); // 66yo is their full retirement age.
+    //const year62 = "2014";
+  const rawEarnings = getRawEarnings(earnings);
+  const userPension = 0;
 
-//   it("Correctly tallies years of substantial earnings from a full earnings record.", async () => {
-//     expect.assertions(1);
+  const piaUserCal = await finalCalculation(
+    userDOB,
+    userDOR,
+    userPension,
+    rawEarnings,
+    null,
+    null,
+    null,null
+  );
+  // var userCalc = await finalCalculation(userDOB, userDOR, year62, userYSE, userPension, userAIME)
 
-//     expect(userYSE).toBe(30);
-//   });
+  const userYSE = Number(piaUserCal["yearsSubstantialEarnings"]);
+
+  it("Correctly tallies years of substantial earnings from a full earnings record.", async () => {
+    expect.assertions(1);
+
+    expect(userYSE).toBe(30);
+  });
+});
 
 //   it("Correctly calculates AIME from a full earnings record.", async () => {
 //     expect.assertions(1);
